d227ade6fc202dab049e738f889865d5ecb4ddda
pesionzhao <pesionzhao@gmail.com>
2025-05-16 05:16:47 +0000

    fix

--- paddle/phi/kernels/gpu/moe_unzip_kernel.cu
+++ paddle/phi/kernels/gpu/moe_unzip_kernel.cu
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
+#include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/moe_unzip_kernel.h"
 #include "paddle/phi/kernels/funcs/moe_utils.h"
 
